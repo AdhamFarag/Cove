1ec506f87be831c155dfef46c883a98f55dd4c2d
Adhamyasser <adham.farag@mail.utoronto.ca>
2022-01-12 19:31:57 -0500

    commit of ML Model and Flask API

---
+++ actualbot.py
@@ -0,0 +1,70 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+# things we need for Tensorflow
+import numpy as np
+import pickle
+import random
+# import tensorflow as tf
+import tflearn
+import pickle
+
+#training_data_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'training_data')
+training_data_file_path = "training_data"
+data = pickle.load( open(training_data_file_path, "rb" ) )
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+# import our chat-bot intents file
+import json
+#JSON_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'dataset\data.json')
+JSON_file_path = "dataset\\data.json"
+with open(JSON_file_path) as json_data:
+    intents = json.load(json_data)
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# logs_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'tflearn_logs')
+model = tflearn.DNN(net, tensorboard_dir='./tflearn_logs')
+
+# load our saved model
+model.load('./model.tflearn')
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+    return(np.array(bag))
+
+
+def response(sentence):
+    '''
+    takes in sentece as input and return back response from the bot based on the data
+    '''
+    results = model.predict([bow(sentence, words)])[0]
+    results_index = np.argmax(results)
+    tag = classes[results_index]
+    for tg in intents["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
+            patterns = tg["patterns"]
+    return(random.choice(responses))
+
---
+++ driver.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template
+from sklearn.metrics import coverage_error
+import json
+from actualbot import response
+app = Flask(__name__)
+
+
+@app.route("/<text>")
+def MainResponse(text):
+    text = text.lower()
+    coveResponse = response(text)
+    print(text)
+    if(coveResponse == None):
+        coveResponse = "I don't think I am Programmed to do that, would you like me to make a note of that"
+    elif("alpha" in text.lower()):
+        # TODO: Handle exit of code here using alpha as a failsafe word
+        pass
+    returnedJSON = app.response_class(
+        response=json.dumps({"status":"success","code":0,"data":coveResponse}),
+        status=200,
+        mimetype='application/json'
+    )
+    return returnedJSON
+
+
+
+def start_server():
+    app.run()
+
+if __name__ == '__main__':
+    start_server()
